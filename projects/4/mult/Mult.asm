dc54e2896cdedef6356f26b465a2e52be64111db
satorukano <kiritomo0206@icloud.com>
2025-03-13 02:07:05 +0900

    add multi asm

--- projects/4/mult/Mult.asm
+++ projects/4/mult/Mult.asm
@@ -8,3 +8,25 @@
 // The algorithm is based on repetitive addition.
 
 //// Replace this comment with your code.
+@R2
+M=0
+@i
+M=1
+(LOOP)
+    @i
+    D=M
+    @R1
+    D=M-D
+    @END
+    D;JLT
+    @R0
+    D=M
+    @R2
+    M=D+M
+    @i
+    M=M+1
+    @LOOP
+    0;JMP
+(END)
+    @END
+    0;JMP
